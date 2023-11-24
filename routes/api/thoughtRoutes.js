538da3fbaa4ca8649fc761a2d285517b285e5c63
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-24 08:37:44 -0800

    deleted comments of the old route structure, implemented post thoughtRoutes to add a thought to a user, format and minor fixes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 // Get one thought by id
 router.get('/:id', async (req, res) => {
     try {
-        const oneThought = await Thought.findOne({ _id: new ObjectId(req.params.id)})
+        const oneThought = await Thought.findOne({ _id: new ObjectId(req.params.id) })
             .select('-__v');
         if (!oneThought) {
             return res.status(404).json({ message: 'No thought found with this id!' });
@@ -34,6 +34,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const newThought = await Thought.create(req.body);
+        const userThought = await User.findOneAndUpdate(
+            { _id: new ObjectId(req.body.userId) },
+            { $addToSet: { thoughts: newThought._id } },
+            { new: true }
+        );
         if (!newThought) {
             return res.status(404).json({ message: 'Creation unsuccessful' });
         }
@@ -47,7 +52,11 @@ router.post('/', async (req, res) => {
 // Update a thought by id
 router.put('/:id', async (req, res) => {
     try {
-        const updateThought = await Thought.findOneAndUpdate({_id: new ObjectId(req.params.id)}, {$set: req.body}, { new: true, runValidators: true });
+        const updateThought = await Thought.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            { $set: req.body },
+            { new: true, runValidators: true }
+        );
         if (!updateThought) {
             return res.status(404).json({ message: 'Update unsuccessful' });
         }
@@ -63,11 +72,18 @@ router.put('/:id', async (req, res) => {
 // Delete a thought by id
 router.delete('/:id', async (req, res) => {
     try {
-        const deleteThought = await Thought.findOneAndDelete({_id: new ObjectId(req.params.id)});
+        const deleteThought = await Thought.findOneAndDelete(
+            { _id: new ObjectId(req.params.id) }
+        );
         if (!deleteThought) {
             return res.status(404).json({ message: 'Delete unsuccessful' });
         }
-        return res.status(200).json(deleteThought);
+        await User.findOneAndUpdate(
+            { username: deleteThought.username },
+            { $pull: { thoughts: req.params._id } },
+            { new: true }
+        );
+        return res.status(200).json(deleteThought, { message: 'Thought deleted' });
     } catch (err) {
         console.log(err);
         return res.status(400).json(err);
@@ -78,7 +94,7 @@ router.delete('/:id', async (req, res) => {
 router.post('/:thoughtId/reactions', async (req, res) => {
     try {
         const addReaction = await Thought.findOneAndUpdate(
-            { _id: new ObjectId(req.params.id) },
+            { _id: new ObjectId(req.params.thoughtId) },
             { $push: { reactions: req.body } },
             { new: true, runValidators: true }
         );
@@ -96,7 +112,7 @@ router.post('/:thoughtId/reactions', async (req, res) => {
 router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
     try {
         const deleteReaction = await Thought.findOneAndUpdate(
-            { _id: new ObjectId(req.params.id) },
+            { _id: new ObjectId(req.params.thoughtId) },
             { $pull: { reactions: { reactionId: new ObjectId(req.params.reactionId) } } },
             { new: true, runValidators: true }
         );
@@ -109,44 +125,5 @@ router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
         return res.status(400).json(err);
     };
 });
-/*
-// Import all of the API routes from /api/index.js
-const {
-    getAllThoughts,
-    getThoughtById,
-    addThought,
-    updateThought,
-    removeThought,
-    addReaction,
-    removeReaction
-    } = require('../../controllers/thoughtController');
-
-// /api/thoughts
-router
-    .route('/')
-    .get(getAllThoughts);
-
-// /api/thoughts/:thoughtId
-router
-    .route('/:thoughtId')
-    .get(getThoughtById)
-    .put(updateThought);
-
-// /api/thoughts/:userId
-router
-    .route('/:userId')
-    .post(addThought);
-
-// /api/thoughts/:userId/:thoughtId
-router
-    .route('/:userId/:thoughtId')
-    .put(addReaction)
-    .delete(removeThought);
-
-// /api/thoughts/:userId/:thoughtId/:reactionId
-router
-    .route('/:userId/:thoughtId/:reactionId')
-    .delete(removeReaction);
-*/
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -56,7 +56,7 @@ router.post('/', async (req, res) => {
 // Update a user by id
 router.put('/:id', async (req, res) => {
     try {
-        const updateUser = await User.findOneAndUpdate({_id: ObjectId(req.params.id)}, {$set: req.body}, { new: true, runValidators: true });
+        const updateUser = await User.findOneAndUpdate({_id: new ObjectId(req.params.id)}, {$set: req.body}, { new: true, runValidators: true });
         if (!updateUser) {
             return res.status(404).json({ message: 'Update unsuccessful' });
         } else {
@@ -122,38 +122,6 @@ router.delete('/:id/friends/:friendId', async (req, res) => {
         return res.status(400).json(err);
     }
 });
-
-/*
-// Import all of the API routes from /api/index.js
-const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    removeFriend
-    } = require('../../controllers/userController');
-
-// /api/users
-router
-    .route('/')
-    .get(getAllUsers)
-    .post(createUser);
-
-// /api/users/:userId
-router
-    .route('/:userId')
-    .get(getUserById)
-    .put(updateUser)
-    .delete(deleteUser);
-
-// /api/users/:userId/friends/:friendId
-router
-    .route('/:userId/friends/:friendId')
-    .put(addFriend)
-    .delete(removeFriend);
-*/
 
 module.exports = router;
 
