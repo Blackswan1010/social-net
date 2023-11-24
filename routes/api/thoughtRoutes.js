fe8437a71e094435200bbea98f6123b1ebc13dcd
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-23 17:07:51 -0800

    implemented both thought and user routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+    } = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts);
+
+// /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought);
+
+// /api/thoughts/:userId
+router
+    .route('/:userId')
+    .post(addThought);
+
+// /api/thoughts/:userId/:thoughtId
+router
+    .route('/:userId/:thoughtId')
+    .put(addReaction)
+    .delete(removeThought);
+
+// /api/thoughts/:userId/:thoughtId/:reactionId
+router
+    .route('/:userId/:thoughtId/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
