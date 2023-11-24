d5fd2c68c51818266c69253e98ba978735695c7a
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-23 23:22:45 -0800

    installed nodemon package, removed controllers dir, restructured userRoutes and thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,115 @@
 const router = require('express').Router();
+const { User, Thought } = require('../../models');
+const { ObjectId } = require('mongodb');
 
+// Get all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.find({})
+            .select('-__v')
+            .sort({ _id: -1 });
+        res.status(200).json(dbThoughtData);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Get one thought by id
+router.get('/:id', async (req, res) => {
+    try {
+        const oneThought = await Thought.findOne({ _id: new ObjectId(req.params.id)})
+            .select('-__v');
+        if (!oneThought) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        return res.status(200).json(oneThought);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Create a thought
+router.post('/', async (req, res) => {
+    try {
+        const newThought = await Thought.create(req.body);
+        if (!newThought) {
+            return res.status(404).json({ message: 'Creation unsuccessful' });
+        }
+        return res.status(200).json(newThought);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Update a thought by id
+router.put('/:id', async (req, res) => {
+    try {
+        const updateThought = await Thought.findOneAndUpdate({_id: new ObjectId(req.params.id)}, {$set: req.body}, { new: true, runValidators: true });
+        if (!updateThought) {
+            return res.status(404).json({ message: 'Update unsuccessful' });
+        }
+        return res.status(200).json(updateThought);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+
+
+// Delete a thought by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteThought = await Thought.findOneAndDelete({_id: new ObjectId(req.params.id)});
+        if (!deleteThought) {
+            return res.status(404).json({ message: 'Delete unsuccessful' });
+        }
+        return res.status(200).json(deleteThought);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Add a reaction to a thought
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const addReaction = await Thought.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            { $push: { reactions: req.body } },
+            { new: true, runValidators: true }
+        );
+        if (!addReaction) {
+            return res.status(404).json({ message: 'Add reaction unsuccessful' });
+        }
+        return res.status(200).json(addReaction);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Delete a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        const deleteReaction = await Thought.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            { $pull: { reactions: { reactionId: new ObjectId(req.params.reactionId) } } },
+            { new: true, runValidators: true }
+        );
+        if (!deleteReaction) {
+            return res.status(404).json({ message: 'Delete reaction unsuccessful' });
+        }
+        return res.status(200).json(deleteReaction);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+/*
 // Import all of the API routes from /api/index.js
 const {
     getAllThoughts,
@@ -37,5 +147,6 @@ router
 router
     .route('/:userId/:thoughtId/:reactionId')
     .delete(removeReaction);
+*/
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,129 @@
 const router = require('express').Router();
+const { User, Thought } = require('../../models');
+const { ObjectId } = require('mongodb');
 
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const dbUserData = await User.find({})
+            .select('-__v')
+            .sort({ _id: -1 });
+        res.status(200).json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Get one user by id
+router.get('/:id', async (req, res) => {
+    try {
+        const oneUser = await User.findOne({ _id: new ObjectId(req.params.id)})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v');
+        if (!oneUser) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        return res.status(200).json(oneUser);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Create a user
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        if (!newUser) {
+            return res.status(404).json({ message: 'Creation unsuccessful' });
+        }
+        return res.status(200).json(newUser);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    };
+});
+
+// Update a user by id
+router.put('/:id', async (req, res) => {
+    try {
+        const updateUser = await User.findOneAndUpdate({_id: ObjectId(req.params.id)}, {$set: req.body}, { new: true, runValidators: true });
+        if (!updateUser) {
+            return res.status(404).json({ message: 'Update unsuccessful' });
+        } else {
+            return res.status(200).json(updateUser);
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    }
+});
+
+// Delete a user by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteUser = await User.findOneAndDelete({_id: new ObjectId(req.params.id)});
+        if (!deleteUser) {
+            return res.status(404).json({ message: 'Delete unsuccessful' });
+        }
+        const deleteThoughts = await Thought.deleteMany({ _id: { $in: deleteUser.thoughts } });
+        if (!deleteThoughts) {
+            return res.status(404).json({ message: 'Deleting thoughts unsuccessful' });
+        } else {
+            return res.status(200).json("User and thoughts deleted");
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    }
+});
+
+// Add a friend to a user
+router.post('/:id/friends/:friendId', async (req, res) => {
+    try {
+        const addFriend = await User.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            { $addToSet: { friends: new ObjectId(req.params.friendId) }},
+            { new: true, runValidators: true }
+        );
+        if (!addFriend) {
+            return res.status(404).json({ message: 'Adding friend was unsuccessful' });
+        }
+        return res.status(200).json(addFriend);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    }
+});
+
+// Remove a friend from a user
+router.delete('/:id/friends/:friendId', async (req, res) => {
+    try {
+        const removeFriend = await User.findOneAndUpdate(
+            { _id: new ObjectId(req.params.id) },
+            { $pull: { friends: new ObjectId(req.params.friendId)} },
+            { new: true }
+        );
+        if (!removeFriend) {
+            return res.status(404).json({ message: 'Removing friend was unsuccessful' });
+        }
+        return res.status(200).json(removeFriend);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+    }
+});
+
+/*
 // Import all of the API routes from /api/index.js
 const {
     getAllUsers,
@@ -29,5 +153,7 @@ router
     .route('/:userId/friends/:friendId')
     .put(addFriend)
     .delete(removeFriend);
+*/
 
 module.exports = router;
+
